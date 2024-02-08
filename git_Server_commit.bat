3d333ed8320320c7fbc038f119ec5aca837b81b4
HiddenMW <qweewqqwe1236@gmail.com>
2024-02-08 23:52:25 +0800

    Server_Archive_2024/02/08 23:52:25.49

--- git_Server_commit.bat
+++ git_Server_commit.bat
@@ -1,11 +1,13 @@
-git add -A**
+git add .
 
-git commit  -m"Server_Archive_%DATE% %TIME%"
+git commit -m"Server_Archive_%DATE% %TIME%"
 
 git branch -M main
 
 git pull origin main
 
 git push -u origin main
+
+echo 上傳成功
 
 pause
